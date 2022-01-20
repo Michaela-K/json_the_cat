7dd2cba31afb4c156ddbb5a977a5d7efbb091822
Michaela-K <michaelakodes@gmail.com>
2022-01-20 19:41:07 +0000

    add breedFetcherTest

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,19 @@
 const request = require("request");
 
-const fetchBreedDescription = function(breed, callback) {   //The first (error) parameter will allow the caller to check and handle error situations easily.
+const fetchBreedDescription = function (breed, callback) {
+  //The first (error) parameter will allow the caller to check and handle error situations easily.
   let url = `https://api.thecatapi.com/v1/breeds/search?name=${breed}`;
   request(url, (error, response, body) => {
-    if(error){
+    if (error) {
       callback(error, null);
     }
     const data = JSON.parse(body);
-    if(data.length === 0){
-    callback(`Please enter a valid kitty name :)`, null);
-    }else{
-    callback(null, data[0].description);
+    if (data.length === 0) {
+      callback(`type not found`, null);
+    } else {
+      callback(null, data[0].description);
     }
   });
 };
-
 
 module.exports = { fetchBreedDescription };
